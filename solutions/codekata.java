bf81b68db50e22b09221465c7202c74d24eb41bb
Preethi Ragupathi <preethirstr@gmail.com>
2026-09-24 18:22:10 +0530

    Update codekata.java

--- solutions/codekata.java
+++ solutions/codekata.java
@@ -1,3 +1,30 @@
+// Problem Statement:
+// Given 2 numbers N and M add both the numbers and check whether the sum is odd or even.
+//
+//
+//
+//
+// Input Description:
+// The input consists of two integers, N and M.
+//
+//
+//
+//
+// Output Description:
+// The output is 'odd' if the sum of N and M is odd, and 'even' if the sum is even.
+//
+//
+//
+//
+// Sample Input:
+// 9 2
+//
+//
+//
+//
+// Sample Output:
+// odd
+
 import java.util.Scanner;
 class Main{
     public static void main(String[] args)
